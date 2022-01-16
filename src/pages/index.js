ad3f501a8f73d2d5a0be717d2dc7ab91eb33a2e8
Lev Shushkov <levashushkov@yandex.ru>
2022-01-16 21:14:51 +0300

    likes fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,21 @@ import UserInfo from "../scripts/components/UserInfo.js";
 
 import { Section } from "../scripts/components/Section";
 
-import { FormValidator } from "../scripts/components/FormValidator.js"
+import { FormValidator } from "../scripts/components/FormValidator.js";
 
 import {
-  mestoAPIConfig,
-  editMestoPopup,
-  buttonAddCard,
-  editProfilePopup,
-  profileButton,
-  avatarPopup,
-  changeAvatarButton,
-  profileName,
-  profileDescription,
-  profileAvatar,
-  imagePopup,
-  validationConfig,
+    mestoAPIConfig,
+    editMestoPopup,
+    buttonAddCard,
+    editProfilePopup,
+    profileButton,
+    avatarPopup,
+    changeAvatarButton,
+    profileName,
+    profileDescription,
+    profileAvatar,
+    imagePopup,
+    validationConfig,
 } from "../scripts/utils/constants.js";
 import { PopupWithImage } from "../scripts/components/PopupWithImage";
 
@@ -44,78 +44,63 @@ Promise.all(initialData)
     .catch((error) => console.log(error))
     .finally(() => {});
 
-
 // Редактирование профиля
 const userInfo = new UserInfo({
-  profileName,
-  profileDescription,
-  profileAvatar,
+    profileName,
+    profileDescription,
+    profileAvatar,
 });
 const changeProfileNamePopup = new PopupWithForm(editProfilePopup, {
-  formSubmitCallBack(data) {
-    apiRyabov
-      .sendProfileDataToServer(data)
-      .then((res) => {
-        userInfo.setUserInfo(res);
-        changeProfileNamePopup.closePopup();
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {});
-  },
+    formSubmitCallBack(data) {
+        apiRyabov
+            .sendProfileDataToServer(data)
+            .then((res) => {
+                userInfo.setUserInfo(res);
+                changeProfileNamePopup.closePopup();
+            })
+            .catch((err) => console.log(err))
+            .finally(() => {});
+    },
 });
 changeProfileNamePopup.setEventListeners();
 
-
-
 //Редактирование аватара
 const changeAvatarImage = new PopupWithForm(avatarPopup, {
-  formSubmitCallBack(data) {
-    console.log(data);
-    apiRyabov
-      .changeAvatarAPI(data.linkAvatarFoto)
-      .then((res) => {
-        console.log(res);
-        userInfo.setUserInfo(res);
-        changeAvatarImage.closePopup();
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {});
-  },
+    formSubmitCallBack(data) {
+        console.log(data);
+        apiRyabov
+            .changeAvatarAPI(data.linkAvatarFoto)
+            .then((res) => {
+                console.log(res);
+                userInfo.setUserInfo(res);
+                changeAvatarImage.closePopup();
+            })
+            .catch((err) => console.log(err))
+            .finally(() => {});
+    },
 });
 changeAvatarImage.setEventListeners();
 
-
 //// Классы валидации форм
 //валидация профайла
-const validatorEditProfilePopup = new FormValidator(
-  validationConfig,
-  editProfilePopup,
-);
+const validatorEditProfilePopup = new FormValidator(validationConfig, editProfilePopup);
 
-console.log(validatorEditProfilePopup);
 //валидация аватара
-const validatorAvatarPopup = new FormValidator(
-  validationConfig,
-  avatarPopup,
-);
+const validatorAvatarPopup = new FormValidator(validationConfig, avatarPopup);
 //валидация новых карточек
-const validatorNewCardPopup = new FormValidator(
-  validationConfig,
-  editMestoPopup,
-);
+const validatorNewCardPopup = new FormValidator(validationConfig, editMestoPopup);
 //активация валидации
 validatorEditProfilePopup.enableValidation();
 validatorAvatarPopup.enableValidation();
 validatorNewCardPopup.enableValidation();
 
-
 //// Слушатели
 // кнопка аватара
 changeAvatarButton.addEventListener("click", () => {
-  changeAvatarImage.openPopup();
+    changeAvatarImage.openPopup();
 });
 // кнопка профайла
 profileButton.addEventListener("click", () => {
-  changeProfileNamePopup.openPopup();
+    changeProfileNamePopup.openPopup();
 });
 // кнопка новой карточки
