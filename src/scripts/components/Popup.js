db9ad389d6f776798af99871c4ded8264ec138a1
Lev Shushkov <levashushkov@yandex.ru>
2022-01-16 21:34:41 +0300

    popup overlay close

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -1,31 +1,43 @@
 export class Popup {
-  constructor(popupElement) {
-    this._popupElement = popupElement;
-    // this._popupOverlay = this._popupElement.querySelector(".popup__overlay");
-    // this._popupWindow = document.querySelector(`#${this.popupElement}`);
-    this._popupCloseButton = this._popupElement.querySelector(".popup__close-icon");
-    this._closeByEscape = this._closeByEscape.bind(this);
-  }
-
-  openPopup() {
-    document.addEventListener("keyup", this._closeByEscape);
-    this._popupElement.classList.add("popup_opened");
-  }
-
-  closePopup() {
-    document.removeEventListener("keyup", this._closeByEscape);
-    this._popupElement.classList.remove("popup_opened");
-  }
-
-  _closeByEscape(event) {
-    if (event.key === "Escape") {
-      this.closePopup();
+    constructor(popupElement) {
+        this._popupElement = popupElement;
+        // this._popupOverlay = this._popupElement.querySelector(".popup__overlay");
+        // this._popupWindow = document.querySelector(`#${this.popupElement}`);
+        this._popupCloseButton = this._popupElement.querySelector(".popup__close-icon");
+        this._closeByEscape = this._closeByEscape.bind(this);
     }
-  }
 
-  setEventListeners() {
-    this._popupCloseButton.addEventListener("click", () => {
-      this.closePopup();
-    });
-  }
+    openPopup() {
+        document.addEventListener("keyup", this._closeByEscape);
+        this._popupElement.classList.add("popup_opened");
+    }
+
+    closePopup() {
+        document.removeEventListener("keyup", this._closeByEscape);
+        this._popupElement.classList.remove("popup_opened");
+    }
+
+    _closeByEscape(event) {
+        if (event.key === "Escape") {
+            this.closePopup();
+        }
+    }
+
+    _closeByOverlayClick(event) {
+        if (event.type === "click") {
+            console.log(event.type);
+            if (event.target === event.currentTarget) {
+                this.closePopup();
+            }
+        }
+    }
+
+    setEventListeners() {
+        this._popupCloseButton.addEventListener("click", () => {
+            this.closePopup();
+        });
+        this._popupElement.addEventListener("click", (event) => {
+            this._closeByOverlayClick(event);
+        });
+    }
 }
