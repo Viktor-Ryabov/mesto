8012a1b46d248658d8cb7b0a15a258c0964f1afc
Рябов Виктор Александрович <ryabov_va@seligdar.local>
2022-01-19 17:17:22 +0300

    last bug in prosses

--- src/scripts/components/Cards.js
+++ src/scripts/components/Cards.js
@@ -25,7 +25,7 @@ export default class Card {
     // }
 
     cardGenerator(element) {
-        this._element = this._cardTemplate;
+        this._element = element;
         this._likeHeart = this._element.querySelector(".card__button-like");
         this._likeCount = this._element.querySelector(".card__number-of-likes");
         this._bucket = this._element.querySelector("#deleteButton");
